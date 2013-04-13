90c33d00068beb4d5ed132baae2fdf5776549a35
Luiz Antonio S Messias <tonyzrp@gmail.com>
2013-04-12 21:59:24 -0300

    the Index controller

---
+++ src/App/Controllers/Index.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use MyTwitter\Controllers\Controller;
+
+/**
+* Index controller
+*/
+class Index extends Controller
+{
+	public function get_index() {
+		echo "minha action index";
+	}	
+}
