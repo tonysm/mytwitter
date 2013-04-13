9f09eef897c6eff298a29e8435852f7ccfd01927
Luiz Antonio S Messias <tonyzrp@gmail.com>
2013-04-13 11:17:52 -0300

    preparing the application to twitter bootstrap, firsts login and register form

--- src/App/Controllers/Index.php
+++ src/App/Controllers/Index.php
@@ -2,15 +2,9 @@
 
 namespace App\Controllers;
 
-use MyTwitter\Controllers\Controller;
-
-/**
-* Index controller
-*/
-class Index extends Controller
+class Index extends AppController
 {
 	public function get_index() {
-		$this->set('teste', 'lorem ipsum');
 		$this->render('index/index');
 	}
 
