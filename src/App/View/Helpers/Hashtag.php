5dcccccadafcefafdfaffa5b0f19f0f19c1b3635
Luiz Antonio S Messias <tonyzrp@gmail.com>
2013-04-13 20:18:34 -0300

    adding a hashtag helper to wrap all hashtags with links, highlighing them..

---
+++ src/App/View/Helpers/Hashtag.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\View\Helpers;
+
+class Hashtag {
+	public function replaceHashtags( $text ){
+		$pattern = "/(#[a-zA-Z0-9]*)/";
+		$replace = '<a href="/messages/find/hash:$1">$1</a>';
+		return preg_replace($pattern, $replace, $text);
+	}
+}
