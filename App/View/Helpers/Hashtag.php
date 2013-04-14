50610db637999d81fce28fe414eb302d3b7f5102
Luiz Antonio S Messias <tonyzrp@gmail.com>
2013-04-13 23:02:22 -0300

    adding some docs

--- App/View/Helpers/Hashtag.php
+++ App/View/Helpers/Hashtag.php
@@ -3,6 +3,11 @@
 namespace App\View\Helpers;
 
 class Hashtag {
+	/**
+	 * wraps all hashtags with a ling to /messages/find/hash:example
+	 * @param string @text
+	 * @return string
+	 */
 	public function replaceHashtags( $text ){
 		$pattern = "/#([a-zA-Z0-9]*)/";
 		$replace = '<a href="/messages/find/hash:$1">#$1</a>';
